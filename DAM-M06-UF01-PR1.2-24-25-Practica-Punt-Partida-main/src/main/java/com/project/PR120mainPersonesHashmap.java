36f0de588534e5b534fe219c0ea4f60c5b024da2
pablo <pablovicenteroura2005@gmail.com>
2024-10-06 17:57:41 +0200

    c

--- DAM-M06-UF01-PR1.2-24-25-Practica-Punt-Partida-main/src/main/java/com/project/PR120mainPersonesHashmap.java
+++ DAM-M06-UF01-PR1.2-24-25-Practica-Punt-Partida-main/src/main/java/com/project/PR120mainPersonesHashmap.java
@@ -2,7 +2,7 @@ package com.project;
 
 import java.io.*;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Map.Entry;
 
 import com.project.excepcions.IOFitxerExcepcio;
 
@@ -38,10 +38,47 @@ public class PR120mainPersonesHashmap {
     // Mètode per escriure les persones al fitxer
     public static void escriurePersones(HashMap<String, Integer> persones) throws IOFitxerExcepcio {
        // *************** CODI PRÀCTICA **********************/
+       try {
+        FileOutputStream fos = new FileOutputStream(filePath);
+
+        ObjectOutputStream oss = new ObjectOutputStream(fos);
+        oss.writeObject(persones);
+        
+        oss.close();
+        fos.close();
+
+       } catch (FileNotFoundException e) {
+        System.out.println("El ficherpo no se encontró: " + e.getMessage());
+       } catch (IOException e) {
+        System.out.println("Error de Entrada/Salida: " + e.getMessage());
+       }
     }
 
     // Mètode per llegir les persones des del fitxer
     public static void llegirPersones() throws IOFitxerExcepcio {
         // *************** CODI PRÀCTICA **********************/
+        try {
+            FileInputStream fis = new FileInputStream(filePath);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            @SuppressWarnings("unchecked")
+            HashMap<String, Integer> persones = (HashMap<String, Integer>) ois.readObject();
+
+            for (Entry<String, Integer> entry : persones.entrySet()) {
+                String nombre = entry.getKey();
+                int edad = entry.getValue();
+                System.out.println(nombre + ": " + edad + " anys");
+            }
+
+            ois.close();
+            fis.close();
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("El ficherpo no se encontró: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error de Entrada/Salida: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("La clase no se encontró: " + e.getMessage());;
+        } 
     }
 }
